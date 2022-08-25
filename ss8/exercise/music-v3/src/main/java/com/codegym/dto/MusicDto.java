915974149442a20a8d85ed8b931b01ceb650dec3
Your Name <you@example.com>
2022-08-25 21:22:34 +0700

    SS

--- ss8/exercise/music-v3/src/main/java/com/codegym/dto/MusicDto.java
+++ ss8/exercise/music-v3/src/main/java/com/codegym/dto/MusicDto.java
@@ -3,11 +3,24 @@ package com.codegym.dto;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class MusicDto implements Validator {
 
     private Integer idSong;
+
+    @Pattern(regexp = "([A-Za-z])+(\\s[A-Za-z])*", message = "Vui lòng nhập tên")
+    @Size(min = 1, max = 800)
     private String nameSong;
+
+    @Pattern(regexp = "(([A-Za-z])+(\\s[A-Za-z])*)", message = "Vui lòng nhập nghệ sĩ")
+    @Size(min = 1, max = 300)
     private String artist;
+
+    @Size(min =1,max = 1000)
+    @Pattern(regexp = "^[\\w ,]+$",message = "Vui lòng nhập thể loại nhạc")
     private String type;
 
     public MusicDto() {
