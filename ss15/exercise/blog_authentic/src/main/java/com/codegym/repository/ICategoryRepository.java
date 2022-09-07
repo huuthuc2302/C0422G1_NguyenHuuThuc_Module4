4a00b48434d7ef24d41ed6a67e79854a3a3afd71
Your Name <you@example.com>
2022-09-07 18:10:03 +0700

    SS

---
+++ ss15/exercise/blog_authentic/src/main/java/com/codegym/repository/ICategoryRepository.java
@@ -0,0 +1,8 @@
+package com.codegym.repository;
+
+import com.codegym.model.CategoryBlog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICategoryRepository extends JpaRepository<CategoryBlog, Integer> {
+
+}
