291196b38b6d28c9774458954b004b552aca7517
Your Name <you@example.com>
2022-09-12 15:10:29 +0700

    SS

--- case-study-m4/src/main/java/com/codegym/controller/EmployController.java
+++ case-study-m4/src/main/java/com/codegym/controller/EmployController.java
@@ -1,4 +1,50 @@
 package com.codegym.controller;
 
+import com.codegym.model.contract.ContractDetail;
+import com.codegym.service.employee.IDivisionService;
+import com.codegym.service.employee.IEducationDegreeService;
+import com.codegym.service.employee.IEmployeeService;
+import com.codegym.service.employee.IPositionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/employees")
 public class EmployController {
+
+    @Autowired
+    private IEmployeeService iEmployeeService;
+
+    @Autowired
+    private IDivisionService iDivisionService;
+
+    @Autowired
+    private IEducationDegreeService iEducationDegreeService;
+
+    @Autowired
+    private IPositionService iPositionService;
+
+    @GetMapping(value = "")
+    public String findAll(Model model,
+                          @PageableDefault(size = 5) Pageable pageable,
+                          @RequestParam Optional<String> keyword) {
+        String keywordVal = keyword.orElse("");
+
+        model.addAttribute("listEmployee",
+                iEmployeeService.findByEmployeeNameContaining(keywordVal, pageable));
+        model.addAttribute("keywordVal", keywordVal);
+
+        model.addAttribute("divisionList", iDivisionService.findAll());
+        model.addAttribute("educationDegreeList", iEducationDegreeService.findAll());
+        model.addAttribute("positionList", iPositionService.findAll());
+        return "/employee/display-employee";
+    }
 }
--- case-study-m4/src/main/java/com/codegym/service/employee/impl/PositionService.java
+++ case-study-m4/src/main/java/com/codegym/service/employee/impl/PositionService.java
@@ -1,4 +1,22 @@
 package com.codegym.service.employee.impl;
 
-public class PositionService {
+import com.codegym.model.employee.Position;
+import com.codegym.repository.employee.IPositionRepository;
+import com.codegym.service.employee.IPositionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PositionService implements IPositionService {
+
+    @Autowired
+    private IPositionRepository iPositionRepository;
+
+    @Override
+    public List<Position> findAll() {
+        return iPositionRepository.findAll();
+    }
+
 }
